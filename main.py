489287012e9ec871a9d9f98f4c433cd34233905b
M01010100 <townsemc@sunypoly.edu>
2025-09-13 22:03:40 -0400

    triangle

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import pygame
 from constants import *
-
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+    DT = 0
+    
     while True:
-        screen.fill("black")
-        pygame.display.flip()
-        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        DT = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
