cbddde6077cecdf8070d066a6a2f09351f5b01db
M01010100 <townsemc@sunypoly.edu>
2025-09-13 22:34:15 -0400

    creation of astrofield, player calss call and print methods for display

--- main.py
+++ main.py
@@ -1,24 +1,44 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
-    DT = 0
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = updatable, drawable
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)    
+
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        screen.fill("black")
-        player.draw(screen)
-        pygame.display.flip()
-        DT = clock.tick(60) / 1000
 
+        updatable.update(dt)
+        screen.fill("black")
+        
+        drawable.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
+
+            pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
